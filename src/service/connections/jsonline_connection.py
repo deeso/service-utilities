24ea4fbd14a7aba311aefb06caa034232e42e9aa
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-01-02 23:16:15 -0600

    adding a json line transport

---
+++ src/service/connections/jsonline_connection.py
@@ -0,0 +1,38 @@
+import json
+from base_connection import ConnectionFactory
+from socket_connection import UDPConnection, TCPConnection
+
+
+class JsonUDPLineConnection(UDPConnection):
+    KEY = "JsonUDPConnection"
+    URI_TYPE = 'udp-json-line'
+
+    def __init__(self, uri, ip_type='ipv4', meta={}, **kargs):
+        host = uri.split(self.URI_TYPE+'://')[1].split(':')[0]
+        port = int(uri.split(self.URI_TYPE+'://')[1].split(':')[0])
+        ConnectionFactory.__init__(self, uri=uri, host=host,
+                                   port=port, **kargs)
+        self.meta = meta
+
+    def send_msg(self, data):
+        try:
+            d = json.dumps(data[:65507])
+            return self.sendto(d)
+        except:
+            raise
+
+
+class JsonTCPLineConnection(TCPConnection):
+    KEY = "JsonTCPConnection"
+    URI_TYPE = 'tcp-json-line'
+
+    def __init__(self, uri, ip_type='ipv4', meta={}, **kargs):
+        host = uri.split(self.URI_TYPE+'://')[1].split(':')[0]
+        port = int(uri.split(self.URI_TYPE+'://')[1].split(':')[0])
+        ConnectionFactory.__init__(self, uri=uri, host=host,
+                                   port=port, **kargs)
+        self.meta = meta
+
+    def send(self, json_data):
+        d = json_data[:65507]
+        return self.sendto(d)
